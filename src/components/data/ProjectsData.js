3ecc9da2567044e749fee1af3b48cb6cc14f23de
Debanjan <debanjanacharya21@gmail.com>
2022-04-22 13:43:09 +0530

    :sparkles: added few things

--- src/components/data/ProjectsData.js
+++ src/components/data/ProjectsData.js
@@ -1,7 +1,7 @@
 export const ProjectData = [
     {
         id: 1,
-        title: "msg.mee Chat-App",
+        title: "Msg.mee:Chat-App",
         about: " Full Stack MERN Chat-App, Coded with ReactJS, MongoDB, Express.JS, Socket.IO, Chakra-UI. Chat with your friends individually or in Group Chat.",
         tags: ["ReactJS", "Chakra UI", "Socket.IO", "NodeJS", "ExpressJS", "MongoDB"],
         demo: "https://msg-mee.herokuapp.com/",
@@ -12,7 +12,7 @@ export const ProjectData = [
     },
     {
         id: 2,
-        title: "Cryptogramm",
+        title: "Cryptogramm:Realtime Crypto-Info",
         about: "Cryptocurrency webapp with ReactJS, ChartJS, Styled with Material-UI and login feature using Google Authentication and firebase",
         tags: ["ReactJS", "Material UI", "Firebase", "ChartJS", "Google Auth"],
         demo: "https://cryptogramm.netlify.app/",
@@ -23,6 +23,15 @@ export const ProjectData = [
     },
     {
         id: 3,
+        title: "Code-Sync:Realtime Code-Editor",
+        about: "Realtime Code-Editor with custom rooms. Utilizing node-js, Socket.IO, styled with simple CSS",
+        tags: ["ReactJS", "Socket.IO", "NodeJS", "CSS"],
+        demo: "https://codee-sync.herokuapp.com/",
+        github: "https://github.com/debanjan-exe/realtime-code-editor",
+        image: "https://github.com/debanjan-exe/realtime-code-editor/raw/master/mockups/mockup4.jpg?raw=true",
+    },
+    {
+        id: 4,
         title: "Crypto-Stats-News",
         about: "Cryptocurrency with real-time price updation along with latest news across the globe. Crafted with ReactJS, Axios, ChartJS, Redux. Used Ant-Design for styling.",
         tags: ["ReactJS", "Ant-Design", "Redux", "ChartJS", "Axios"],
@@ -33,8 +42,8 @@ export const ProjectData = [
 
     },
     {
-        id: 4,
-        title: "Music Player",
+        id: 5,
+        title: "Music-Player:Grove On It",
         about: "A Simple looking Music Player web-app, Used ReactJS, Vanilla JS. Applied SASS for UI and well responsive",
         tags: ["ReactJS", "CSS", "SASS", "VanillaJS"],
         demo: "https://debanjan-exe.github.io/music-player-webapp/",

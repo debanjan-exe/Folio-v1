aa607d1703d94435e513c38168cb02b51583dde7
Debanjan <debanjanacharya21@gmail.com>
2022-04-27 17:38:22 +0530

    :green_heart: modified few things

--- src/components/data/ProjectsData.js
+++ src/components/data/ProjectsData.js
@@ -2,7 +2,7 @@ export const ProjectData = [
     {
         id: 1,
         title: "Netflix:The-Clone",
-        about: " Clone of the popular OTT 'Netflix' structured using Next.JS, Stripe API for Payments and styled using Taiwind & Material UI, firebase for DB and finally deployed on Vercel",
+        about: " Clone of the popular OTT 'Netflix' structured using Next.JS, Stripe API for Payments and styled using Tailwind & Material UI, firebase for DB and finally deployed on Vercel",
         tags: ["NextJS", "Stripe", "Firebase", "Typescript", "Taiwind", "Material UI"],
         demo: "http://netflix-clone-v1.vercel.app/",
         github: "https://github.com/debanjan-exe/netflix-clone-v1",

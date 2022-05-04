999089662f0172c710471632cb61ded0d01469cb
Debanjan <debanjanacharya21@gmail.com>
2022-05-04 07:17:35 +0530

    :fire:

--- src/components/data/ProjectsData.js
+++ src/components/data/ProjectsData.js
@@ -2,7 +2,7 @@ export const ProjectData = [
     {
         id: 1,
         title: "Netflix:The-Clone",
-        about: " Clone of the popular OTT 'Netflix' structured using Next.JS, Stripe API for Payments and styled using Tailwind & Material UI, firebase for DB and finally deployed on Vercel",
+        about: " Clone of the popular OTT 'Netflix' structured using Next.JS, Stripe API for Payments and styled using Tailwind & Material UI, firebase for DB and finally deployed on Vercel.",
         tags: ["NextJS", "Stripe", "Firebase", "Typescript", "Taiwind", "Material UI"],
         demo: "http://netflix-clone-v1.vercel.app/",
         github: "https://github.com/debanjan-exe/netflix-clone-v1",
@@ -35,7 +35,7 @@ export const ProjectData = [
     {
         id: 4,
         title: "Code-Sync:Realtime Code-Editor",
-        about: "Realtime Code-Editor with custom rooms and users list. Utilizing technologies like Node-js for backend, realtime communication with Socket.io, responsively styled with CSS",
+        about: "Realtime Code-Editor with custom rooms and users list. Utilizing technologies like Node-js for backend, realtime communication with Socket.io, responsively styled with CSS.",
         tags: ["ReactJS", "Socket.IO", "NodeJS", "ExpressJS", "CSS", "Codemirror-API"],
         demo: "https://codee-sync.herokuapp.com/",
         github: "https://github.com/debanjan-exe/realtime-code-editor",
@@ -56,7 +56,7 @@ export const ProjectData = [
         id: 6,
         title: "Music-Player:Grove On It",
         about: "A Simple looking Music Player web-app with aesthetic and interactive well responsive UI, styled using SASS & CSS",
-        tags: ["ReactJS", "CSS", "SASS", "VanillaJS", "Responsive-Web-Design"],
+        tags: ["ReactJS", "CSS", "SASS", "VanillaJS", "Responsive-Web-Design."],
         demo: "https://debanjan-exe.github.io/music-player-webapp/",
         github: "https://github.com/debanjan-exe/music-player-webapp",
         image: "https://github.com/debanjan-exe/music-player-webapp/blob/master/mockups/music-player-webapp-5.jpg?raw=true",
